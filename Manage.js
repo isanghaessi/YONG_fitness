ec4b0f7493870333ef661e15cebb70c78eeb2922
isanghasessi <jesse1231@naver.com>
2020-11-30 00:12:15 +0900

    express-generator

--- Manage.js
+++
@@ -1,8 +0,0 @@
-var http=require('http');
-
-var server=http.createServer();
-
-var port=5000;
-server.listen(port, function() {
-              console.log('server started: %d',port);
-              });
