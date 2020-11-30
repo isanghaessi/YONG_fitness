1ab160a79d1abd9a8cf53eced3d674b0284eb41f
isanghasessi <jesse1231@naver.com>
2020-11-30 23:06:33 +0900

    sync_1

--- server/routes/route.js
+++ server/routes/route.js
@@ -36,13 +36,13 @@ router.get('/login', function(req, res, next){
     pool.query('select * from Administrator where Id=? and Pw=?', [Id, Pw], function(err, results){
         if(err) throw err;
         if(results.length>0){
-            pool.query('select * from Member', function(err, results){
+            pool.query('select Member.mId, Member.name, birthDate, Member.type, startDate, endDate, Trainer.name trainer from Member, Trainer where Member.mId=Trainer.mId union select Member.mId, Member.name, birthDate, Member.type, startDate, endDate, null from Member, Trainer where (Member.mId) not in (select mId from Trainer) order by mId', function(err, results){
                 if(err) throw err;
                 res.locals.mFormat=function(date){
                     return moment(date).format('yyyy-MM-DD')
                 };
                 res.render('main_member', {data:results, warn:''});
-            })
+            });
         }else{
             res.render('login', {data:'wrong ID/PW'});
         }
@@ -65,7 +65,7 @@ router.get('/signin_add', function(req, res, next) {
                 pool.query('insert into Administrator values (?, ?, ?)', [Id, Pw, type], function(err, results){
                     if(err) throw err;
                 });
-                res.render('main', {data:results});
+                res.render('login', {data: ''});
             }else{
                 res.render('signin',{data:'type all data'});
             }
@@ -75,35 +75,53 @@ router.get('/signin_add', function(req, res, next) {
     });
 });
 
-router.get('/to_tainer', function(req, res, next){
-    pool.query('select * from Trainer', function(err, results){
-        if(err) throw err;
+router.get('/search_member', function(req, res, next){
+    var input=req.param(input);
+    pool.query('select * from (select Member.mId, Member.name, birthDate, Member.type, startDate, endDate, Trainer.name trainer from Member, Trainer where Member.mId=Trainer.mId union select Member.mId, Member.name, birthDate, Member.type, startDate, endDate, null from Member, Trainer where (Member.mId) not in (select mId from Trainer)) where ?=mId || ?=name || ?=birthDate || ?=type || ?=startDate || ?=endDate || ?=trainer order by mId', [input, input, input, input, input, input, input], function(err, results){
+        if (err) throw err;
         if(results.length>0){
-            res.render('main_trainer',{data: results, warn:''});
+            res.render('main_member', {data: results, warn: ''})
         }
     });
-})
+});
+
+router.get('/to_trainer', function(req, res, next){
+    pool.query('select tId, Trainer.name, Trainer.type, Member.name mem from Trainer, Member where Trainer.mId=Member.mId or der by tId', function(err, results){
+        if(results.length>0){
+            res.render('main_trainer',{data: results, warn: ''});
+        }
+    });
+});
 
 router.get('/member_modify', function(req, res, next){
     var mId=req.param("mId");
     pool.query('select * from Member where mId=?', [mId], function(err, results){
         if(err) throw err;
         if(results.length>0){
-            var mId=req.param('mId');
+            var name=results[0].name;
             var birthDate=req.param('birthDate');
             var type=req.param('type');
             var startDate=req.param('startDate');
             var endDate=req.param('endDate');
             var trainer=rew.param('trainer');
             if(type==null || startDate==null || endDate==null){
-                pool.query('select * from Member where mId=?', [mId], function(err, results){
-                    res.render('main_member',{data: results, warn: 'input correctly'});
-                });
+                pool.query('select distinct(*) from Train, Member where Train.mId=Member.mId', function(err, results){
+                if(err) throw err;
+                res.render('main_member',{data: results, warn: input_correnctly});
+            });
             }
-            pool.query('update Member set birthDate=?, type=?, startDate=?, endDate=?, trainer=? where (select * from Member from mId=?)',[birthDate, type, startDate, endDate, trainer], function(err, results){
+            pool.query('update Member set birthDate=?, type=?, startDate=?, endDate=? where (select * from Member from mId=?)',[birthDate, type, startDate, endDate, mId], function(err, results){
                 if(err) throw err;
             });
-            pool.query('select * from Member', function(err, results){
+            var tId;
+             pool.query('select * from Trainer where name=?', [trainer], function(err, results){
+                if(err) throw err;
+                tId=results[0].tId;
+            });
+            pool.query('update Train set tId=?, trainer=?, mId=?, member=? where tId=? and mId=?',[tId, trainer, mId, name, tId, mid], function(err, results){
+                if(err) throw err;
+            })
+            pool.query('select distinct(*) from Train, Member where Train.mId=Member.mId', function(err, results){
                 if(err) throw err;
                 res.render('main_member',{data: results, warn: ''});
             });
@@ -117,55 +135,90 @@ router.get('/member_delete', function(req, res, next){
     pool.query('delete from Member where mId=?', [mId], function(err, results){
         if(err) throw err;
     });
+    pool.query('select distinct(*) from Train, Member where Train.mId=Member.mId', function(err, results){
+        if(err) throw err;
+        res.render('main_member',{data:results, warn:""});
+    });
 });
 
 router.get('/new_member', function(req, res, next){
+        var name=req.param("name");
         var birthDate=req.param('birthDate');
         var type=req.param('type');
         var startDate=req.param('startDate');
         var endDate=req.param('endDate');
         var trainer=rew.param('trainer');
         if(type==null || startDate==null || endDate==null){
-            pool.query('select * from Member where mId=?', [mId], function(err, results){
-                res.render('new_member',{data: results, warn: 'input correctly'});
-            });
+            res.render('new_member',{data: results, warn: 'input correctly'});
         }
         var mId;
         pool.query('select count(*) from Member', function(err, results){
             if(err) throw err;
-            mId=results+1;
-        })
-        pool.query('insert into Member values (?, ?, ?, ?, ?, ?)',[mId, birthDate, type, startDate, endDate, trainer], function(err, results){
+            mId=results[0]+1;
+        });
+        pool.query('insert into Member values (?, ?, ?, ?, ?)',[name, mId, birthDate, type, startDate, endDate], function(err, results){
             if(err) throw err;
-                pool.query('select * from Member', function(err, results){
-            if(err) throw err;
-                res.render('main_member',{data: results, warn: 'add complete'});
-            });
+        });
+        poolquery('select * from Trainer name=?', [trainer], function(err, results){
+            if(err) return err;
+            if(results.length>0){
+                pool.query('insert into Train values (?, ?, ?, ?, ?)', [results.tId, trainer, mId, name, type],function(err, results){
+                    if(err) throw err;
+                    pool.query('select distinct(*) from Train, Member where Train.mId=Member.mId', function(err, results){
+                        if(err) throw err;
+                        res.locals.mFormat=function(date){
+                            return moment(date).format('yyyy-MM-DD')
+                        };
+                        res.render('main_member', {data:results, warn:''});
+                    });
+                })
+            }else{
+                res.render('new_member', {warn: "trainer doesn't exist"});
+            }
         });
 });
 
+router.get('/search_trainer', function(req, res, next){
+    var search=req.param(input);
+    pool.query('select distince(*) from Trainer, Member where Trainer.mId=Member.mId and mId=? or name=? of type=? or trainer=?', [input, inout, input, input], function(err, results){
+        if (err) throw err;
+        if(results.length>0){
+            res.render('main_trainer', {data: results, warn: ''})
+        }
+    });
+});
 
-
-
-
+router.get('/to_member', function(req, res, next){
+    pool.query('select Member.mId, Member.name, birthDate, Member.type, startDate, endDate, Trainer.name trainer from Member, Trainer where Member.mId=Trainer.mId union select Member.mId, Member.name, birthDate, Member.type, startDate, endDate, null from Member, Trainer where (Member.mId) not in (select mId from Trainer) order by mId', function(err, results){
+                if(err) throw err;
+                res.locals.mFormat=function(date){
+                    return moment(date).format('yyyy-MM-DD')
+                };
+                res.render('main_member', {data:results, warn:''});
+            });
+});
 
 router.get('/trainer_modify', function(req, res, next){
     var tId=req.param("tId");
     pool.query('select * from Trainer where tId=?', [tId], function(err, results){
         if(err) throw err;
         if(results.length>0){
-            var tId=req.param('tId');
+            var name=results[0].name;
             var type=req.param('type');
             var member=rew.param('member');
             if(type==null){
-                pool.query('select * from Trainer where tId=?', [tId], function(err, results){
-                    res.render('main_trainer',{data: results, warn: 'input correctly'});
-                });
+                pool.query('select distinct(*) from Train, Member where Train.mId=Member.mId', function(err, results){
+                if(err) throw err;
+                res.render('main_trainer',{data: results, warn: input_correnctly});
+            });
             }
-            pool.query('update Member set birthDate=?, type=?, startDate=?, endDate=?, trainer=? where (select * from Member from mId=?)',[birthDate, type, startDate, endDate, trainer], function(err, results){
+            pool.query('update Member tId=?, type=?, name=? where (select * from Member from mId=?)',[tId, type, name, mId], function(err, results){
                 if(err) throw err;
             });
-            pool.query('select * from Member', function(err, results){
+            pool.query('update Train set tId=?, trainer=?, mId=?, member=? where tId=? and mId=?',[tId, name, mId, member, tId, mid], function(err, results){
+                if(err) throw err;
+            })
+            pool.query('select distinct(*) from Train, Member where Train.mId=Member.mId', function(err, results){
                 if(err) throw err;
                 res.render('main_member',{data: results, warn: ''});
             });
@@ -174,36 +227,46 @@ router.get('/trainer_modify', function(req, res, next){
 });
 
 
-router.get('/member_delete', function(req, res, next){
-    var mId=req.param("mId");
-    pool.query('delete from Member where mId=?', [mId], function(err, results){
+router.get('/trainer_delete', function(req, res, next){
+    var tId=req.param("tId");
+    pool.query('select distinct(*) from Train, Member where Train.mId=Member.mId', function(err, results){
         if(err) throw err;
+        res.render('main_trainer',{data:results, warn:""});
     });
 });
 
-router.get('/new_member', function(req, res, next){
-        var birthDate=req.param('birthDate');
+router.get('/new_trainer', function(req, res, next){
+        var name=req.param("name");
         var type=req.param('type');
-        var startDate=req.param('startDate');
-        var endDate=req.param('endDate');
-        var trainer=rew.param('trainer');
-        if(type==null || startDate==null || endDate==null){
-            pool.query('select * from Member where mId=?', [mId], function(err, results){
-                res.render('new_member',{data: results, warn: 'input correctly'});
-            });
+        var member=rew.param('member');
+        if(type==null){
+            res.render('new_member',{data: results, warn: 'input correctly'});
         }
-        var mId;
-        pool.query('select count(*) from Member', function(err, results){
+        var tId;
+        pool.query('select count(*) from Trainer', function(err, results){
             if(err) throw err;
-            mId=results+1;
+            tId=results[0]+1;
         })
-        pool.query('insert into Member values (?, ?, ?, ?, ?, ?)',[mId, birthDate, type, startDate, endDate, trainer], function(err, results){
+        pool.query('insert into Trainer values (?, ?, ?, ?, ?)',[name, tId, type], function(err, results){
             if(err) throw err;
-                pool.query('select * from Member', function(err, results){
-            if(err) throw err;
-                res.render('main_member',{data: results, warn: 'add complete'});
-            });
         });
+        poolquery('select * from member where name=?', [member], function(err, results){
+            if(err) return err;
+            if(results.length>0){
+                pool.query('insert into Train values (?, ?, ?, ?, ?)', [results.tId, trainer, mId, name, type],function(err, results){
+                    if(err) throw err;
+                    pool.query('select distinct(*) from Train, Member where Train.mId=Member.mId', function(err, results){
+                        if(err) throw err;
+                        res.locals.mFormat=function(date){
+                            return moment(date).format('yyyy-MM-DD')
+                        };
+                        res.render('main_trainer', {data:results, warn:''});
+                    });
+                })
+            }else{
+                res.render('new_member', {warn: "member doesn't exist"});
+            }
+        })
 });
 
 module.exports = router;
--- server/sql/Init_db.sql
+++ server/sql/Init_db.sql
@@ -1,6 +1,5 @@
-drop table if exists Train;
-drop table if exists Member;
 drop table if exists Trainer;
+drop table if exists Member;
 drop table if exists Administrator;
 drop table if exists Master;
 
@@ -16,13 +15,6 @@ CREATE TABLE Administrator (
     PRIMARY KEY (Id(255), type(255))
 );
 
-create table Trainer(
-	name text,
-    tId int not null,
-    type text,
-    primary key (tId)
-);
-
 CREATE TABLE Member (
     name TEXT,
     mId INT NOT NULL,
@@ -33,17 +25,15 @@ CREATE TABLE Member (
     PRIMARY KEY (mId)
 );
 
-create table Train(
-	tId int,
-    trainer text,
-    mId int not null,
-    member text,
+create table Trainer(
+	name text,
+    tId int not null,
     type text,
-    foreign key(tId) references Trainer(tId) on delete set null,
-    foreign key(mId) references Member(mId) on delete cascade
+    mId int,
+    foreign key(mId) references Member(mId),
+    primary key (tId, mId)
 );
 
-delete from Train;
 delete from Member;
 delete from Trainer;
 delete from Administrator;
@@ -52,11 +42,14 @@ delete from Master;
 insert into Master values ('0000');
 
 insert into Administrator values('yong', '0000', 'boss');
-insert into Trainer values('Mike', 1, 'Weight');
-insert into Trainer values('Susan', 2, 'Swim');
-insert into Trainer values('Sam', 3, 'Squash');
 
 insert into Member values('Alfred', 1, '1996-12-31', 'All', '2020-11-29', '2021-11-29');
 insert into Member values('Son', 2, '1994-03-28', 'Weight', '2020-10-15', '2021-01-15');
 insert into Member values('Son', 3, '1986-01-22', 'Swim', '2020-03-01', '2021-03-01');
 insert into Member values('Minho', 4, '2000-02-13', 'Squash', '2019-05-01', '2022-05-01');
+insert into Member values('Jack', 5, '2001-12-13', 'Weight', '2020-05-01', '2020-08-01');
+
+insert into Trainer values('Mike', 1, 'Weight', 2);
+insert into Trainer values('Mike', 1, 'Squash', 4);
+insert into Trainer values('Susan', 2, 'Swim', 3);
+insert into Trainer values('Sam', 3, 'Squash', 1);
